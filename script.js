564e8db9f8e7e97e5059b908429ce664ebb2d41f
saff-it <saff_it@ymail.com>
2022-06-13 15:46:38 +0200

    exercise done, but spaces beetwen words to be fixed

---
+++ script.js
@@ -0,0 +1,25 @@
+//richiesta nome utente
+
+let userName = prompt('Qual è il tuo nome?');
+
+document.getElementById('user-name').innerHTML =userName;
+
+console.log(userName);
+
+
+//richiesta cognome utente
+
+let userLastName = prompt('E il tuo cognome?');
+
+document.getElementById('user-lastname').innerHTML =userLastName;
+
+console.log(userLastName);
+
+
+//richiesta colore preferito
+
+let userFavColor = prompt('Mi diresti il tuo colore preferito?');
+
+document.getElementById('user-fav-color').innerHTML =userFavColor;
+
+console.log(userFavColor);
